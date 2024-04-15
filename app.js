f7ffadcb1b377089caa0230d568847acaf98941a
Musma-n <154560114+Musma-n@users.noreply.github.com>
2024-04-15 12:48:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let inputForm = document.querySelector("#table");
+let input = document.querySelector("#input");
+let table = document.querySelector("#timetable");
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let tableOf = input.value;
+
+  table.innerHTML = "";
+
+  for (let i = 1; i <= 10; i++) {
+    const message = `${tableOf} X ${i} = ${tableOf * i}`;
+
+    table.innerHTML += `${message} <br>`;
+  }
+});
